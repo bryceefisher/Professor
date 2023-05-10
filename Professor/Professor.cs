7630aad1de09ecfa2809f8586d080dcaeddd3160
Bryce Fisher <bryce.e.fisher@gmail.com>
2023-05-09 20:33:42 -0700

    Initial commit

---
+++ Professor/Professor.cs
@@ -0,0 +1,66 @@
+namespace Professor;
+
+public class Professor
+{
+    //instance variables
+    private int _lNumber;
+    private string _fName;
+    private string _lName;
+    private string _department;
+        
+    //constructors
+    public Professor()
+    {
+    }
+
+    public Professor(int lNumber, string fName, string lName, string dept)
+    {
+        _lNumber = lNumber;
+        _fName = fName;
+        _lName = lName;
+        _department = dept;
+    }
+        
+    //properties
+    public int LNumber
+    {
+        get { return _lNumber; }
+        set
+        {
+            if (value.ToString().Length == 9)
+            {
+                _lNumber = value;
+            }
+            else
+            {
+                throw new ArgumentException("LNumber must be 8 characters long");
+            }
+
+        }
+    }
+    
+    public string FirstName
+    {
+        get { return _fName; }
+        set { _fName = value; }
+    }
+    
+    public string LastName
+    {
+        get { return _lName; }
+        set { _lName = value; }
+    }
+    
+    public string Department
+    {
+        get { return _department; }
+        set { _department = value; }
+    }
+
+    //methods
+        
+    public override string ToString()
+    {
+        return $"LNumber: {_lNumber}, Name: {_fName}, Last Name: {_lName}, Deptartment: {_department}";
+    }
+}
---
+++ Professor/Program.cs
@@ -0,0 +1,68 @@
+﻿using Professor;
+
+namespace Professor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Professor test methods
+            TestConstructors();
+            Console.WriteLine("--------------------------------------------------------------------");
+            TestToString();
+            Console.WriteLine("--------------------------------------------------------------------");
+            TestPropertyGetters();
+            Console.WriteLine("--------------------------------------------------------------------");
+            TestPropertySetters();
+            
+
+        }
+        //test both constructors
+        static void TestConstructors()
+        {
+            Professor p1 = new Professor();
+            Professor p2 = new Professor(123456789, "John", "Doe", "Computer Science");
+
+            Console.WriteLine("Expecting default values: " + p1.ToString());
+            Console.WriteLine("Overloaded Constructor: " + p2.ToString());
+        }
+        //test ToString
+        static void TestToString()
+        {
+            Professor p1 = new Professor(123456789, "John", "Doe", "Computer Science");
+
+            Console.WriteLine("Expecting 123456789, John, Doe, Computer Science: " + p1.ToString());
+        }
+        //test getters
+        static void TestPropertyGetters()
+        {
+            Professor p1 = new Professor(123456789, "John", "Doe", "Computer Science");
+
+            Console.WriteLine("Expecting 123456789: " + p1.LNumber);
+            Console.WriteLine("Expecting John: " + p1.FirstName);
+            Console.WriteLine("Expecting Doe: " + p1.LastName);
+            Console.WriteLine("Expecting Computer Science: " + p1.Department);
+        }
+        //test setters
+        static void TestPropertySetters()
+        {
+            Professor p1 = new Professor(123456789, "John", "Doe", "Computer Science");
+
+            p1.LNumber = 987654321;
+            p1.FirstName = "Jane";
+            p1.LastName = "Smith";
+            p1.Department = "Math";
+
+            Console.WriteLine("Expecting 987654321: " + p1.LNumber);
+            Console.WriteLine("Expecting Jane: " + p1.FirstName);
+            Console.WriteLine("Expecting Smith: " + p1.LastName);
+            Console.WriteLine("Expecting Math: " + p1.Department);
+            Console.WriteLine("--------------------------------------------------------------------");
+            //test setter validation
+            Console.WriteLine("Testing Setter Validation");
+            Console.WriteLine("Expecting error: ");
+            p1.LNumber = 12345678;
+        }
+        
+    }
+}
